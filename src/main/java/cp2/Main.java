96169da3b34d76c91a84820dc7a1ea351a9378c0
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-31 18:13:28 -0500

    Temp commit

---
+++ src/main/java/cp2/Main.java
@@ -0,0 +1,10 @@
+package main.java.cp2;
+
+/**
+ * Created by Hernan Y.Ke on 2015/12/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+}
---
+++ src/test/java/cp2/MainTest.java
@@ -0,0 +1,13 @@
+package test.java.cp2;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Hernan Y.Ke on 2015/12/27.
+ */
+public class MainTest {
+    @Test
+    public void testG(){
+        assertEquals("3","3");
+    }
+}
